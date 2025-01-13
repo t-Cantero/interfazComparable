98e94d988217eb5811cf4032e241c06efc11d614
tcanterorodriguez <antcanrod@alu.edu.gva.es>
2025-01-13 19:54:56 +0100

    Parentesis finalizado

--- src/Parentesis.java
+++ src/Parentesis.java
@@ -1,32 +1,32 @@
 import java.util.Stack;
 
 public class Parentesis {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         String frase = Utilidades.leerCadena("Introduce una cadena: ");
         Stack parentesis = new Stack();
         char caracter;
-        for(int i = 0;i<frase.length();i++){
-            if(frase.charAt(i) == '(' || frase.charAt(i)=='[' || frase.charAt(i)=='{'){
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == '(' || frase.charAt(i) == '[' || frase.charAt(i) == '{') {
                 parentesis.push(frase.charAt(i));
             }
-            if(frase.charAt(i) == ')' || frase.charAt(i)==']'|| frase.charAt(i)=='}'){
+            if (frase.charAt(i) == ')' || frase.charAt(i) == ']' || frase.charAt(i) == '}') {
                 caracter = frase.charAt(i);
-                if(!comprobar(parentesis,caracter)){
+                if (!comprobar(parentesis, caracter)) {
                     System.out.println("Está mal parentizada");
                     break;
-                }else{
-                    if(parentesis.isEmpty()){
+                } else {
+                    if (parentesis.isEmpty()) {
 
-                    }else{
+                    } else {
                         parentesis.pop();
                     }
                 }
 
             }
-            if(i == frase.length()-1){
-                if(parentesis.isEmpty()){
+            if (i == frase.length() - 1) {
+                if (parentesis.isEmpty()) {
                     System.out.println("!Todo Correcto!");
-                }else{
+                } else {
                     System.out.println("Está mal parentizada");
                 }
             }
@@ -34,12 +34,27 @@ public class Parentesis {
 
     }
 
-    static boolean comprobar(Stack parentesis, char caracter){
-        char ultimoElemento = (char) parentesis.peek();
+    static boolean comprobar(Stack parentesis, char caracter) {
         boolean decision = true;
-        if(caracter == ']'){
-            if(ultimoElemento != '['){
-                decision = false;
+        if (parentesis.isEmpty()) {
+            decision = false;
+        } else {
+            char ultimoElemento = (char) parentesis.peek();
+
+            if (caracter == ']') {
+                if (ultimoElemento != '[' || parentesis.isEmpty()) {
+                    decision = false;
+                }
+            }
+            if (caracter == ')') {
+                if (ultimoElemento != '(' || parentesis.isEmpty()) {
+                    decision = false;
+                }
+            }
+            if (caracter == '}') {
+                if (ultimoElemento != '{' || parentesis.isEmpty()) {
+                    decision = false;
+                }
             }
         }
         return decision;
