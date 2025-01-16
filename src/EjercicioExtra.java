35983add80c3558741f275409333b660095135bd
tcanterorodriguez <antcanrod@alu.edu.gva.es>
2025-01-16 16:24:16 +0100

    Ejercicio extra

--- src/EjercicioExtra.java
+++ src/EjercicioExtra.java
@@ -3,19 +3,16 @@ import java.util.stream.Collectors;
 
 public class EjercicioExtra {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(7);
-        list.add(4);
-        list.add(2);
-        list.add(6);
+        List<Integer> values = new ArrayList<>();
+        values.add(3);
+        values.add(2);
+        values.add(17);
+        values.add(6);
+        values.add(8);
 
-        List<Integer> values = list.stream()
-                .filter(value -> value > 5)
-                .map(value -> value * 2)
-                .collect(Collectors.toList());
-
-        System.out.println(values);
+        values.stream()
+                .filter(value -> value % 2 == 0)
+                .forEach(value -> System.out.println(value));
 
     }
 }
